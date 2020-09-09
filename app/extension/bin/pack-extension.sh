6a5da682260e71ecf3ed23dd2baa28a8052d7d2f
javascpdev <kingpower1990115@gmail.com>
2020-09-09 07:03:27 -0600

    cleanup

--- app/extension/bin/pack-extension.sh
+++ app/extension/bin/pack-extension.sh
@@ -4,13 +4,7 @@ echo $EXTENSION_PEM_BASE64 | base64 -d > /app/extension/bin/extension.pem || tru
 
 mkdir /app/pixels.chrisesplin.com/public/extension || true
 
-ls -al /app/pixels.chrisesplin.com/public
-ls -al /app/pixels.chrisesplin.com/public/extension
-
 node /app/extension/bin/pack-extension.js
 zip -r /app/pixels.chrisesplin.com/public/extension/pixels.zip /app/extension/build
 
 cp -r /app/pixels.chrisesplin.com/public/extension /app/pixels.chrisesplin.com/out || true
-
-ls -al /app/pixels.chrisesplin.com/public/extension
-ls -al /app/pixels.chrisesplin.com/out/extension
